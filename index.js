15a93ab6eed2e62da07d6a128b8834a551472026
Abeeb Adeniyi <145553438+abiolayoung@users.noreply.github.com>
2023-12-24 13:48:04 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,47 @@
+//Logical Operators
+//there are 4 types of logical operators in javascript. 0R (||), AND (&&), NOT (!), NULLISH COALESCING (??)
+//OR OPERATOR
+// True || True => true
+// true || false => true
+// false || true => true
+// false || false => false
+
+//AND OPERATOR
+// True || True => true
+// true || false => false
+// false || true => false
+// false || false => false
+
+//NOT OPERATOR
+
+// !true => false
+// !false => true
+
+// let a = 23
+// let b = 34
+// let c = 45
+// if (a == 23 || b == 34 || c == 45){
+//     alert("True Returned")
+// } else{
+//     alert("false returned")
+// }
+
+// NULLISH COALESCING OPERATOR
+// a??b =>  if a is defined then a else b
+// let user;
+// alert(user??"sherrymoney")
+
+// let firstName = null
+// let lastName = null
+// let nickName = 'abiolayoung'
+// alert(firstName??lastName??nickName??"abiolayoungwebdev")
+
+// Alert, prompt, and confirm
+// prompt(title, [default])
+// alert("Hello World")
+// let result = confirm(question)
+
+// let isInlove = confirm('are you in love with adams?');
+// alert(isInlove)
+
+
